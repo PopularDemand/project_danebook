218a896b3c1876f243b8d0f460184d3c50cf2cc9
Alexa <alexa.anderson@outlook.com>
2016-12-11 13:43:58 -0600

    Add friend info to layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 
+  skip_before_action :require_logged_in_user, only: [:create]
+
   def create
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
